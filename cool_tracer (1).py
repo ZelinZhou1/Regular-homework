ac4929518bc975244c4f0b4bb650b974b6d4c102
ZelinZhou1 <2594871384@qq.com>
2026-02-04 12:37:16 +0800

    Add files via upload
    带递归深度

---
+++ cool_tracer (1).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from sys import monitoring
+
+depth = 0
+def on_start(code, offset):
+    global depth
+    depth += 1
+    prefix = depth * ">"
+    print(f"{prefix} entering {code.co_name}")
+
+def on_return(code, offset, retval):
+    global depth
+    prefix = depth * "<"
+    depth -= 1
+    print(f"{prefix} leaving {code.co_name}, return value: {retval}")
+
+ID = 0
+monitoring.use_tool_id(ID, "cool")
+monitoring.register_callback(ID, monitoring.events.PY_START, on_start)
+monitoring.register_callback(ID, monitoring.events.PY_RETURN, on_return)
+
+def fib(i):
+    if i <= 2:
+        return 1
+    else:
+        return fib(i-1) + fib(i-2)
+
+monitoring.set_events(ID, monitoring.events.PY_START|monitoring.events.PY_RETURN)
+fib(5)
+monitoring.set_events(ID, 0)
+monitoring.free_tool_id(ID)
